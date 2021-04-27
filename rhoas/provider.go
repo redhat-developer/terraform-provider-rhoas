a8def0f96ccf546929fc28209c453050c64d297e
Wojciech Trocki <wtrocki@redhat.com>
2021-04-27 18:12:28 +0100

    fix: make token non optional
    I might not understand it but I do not see how token could be optional here

--- rhoas/provider.go
+++ rhoas/provider.go
@@ -3,6 +3,7 @@ package rhoas
 import (
 	"context"
 	"fmt"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"redhat.com/rhoas/rhoas-terraform-provider/m/rhoas/cli/config"
@@ -13,39 +14,39 @@ import (
 )
 
 const (
-	DefaultAuthURL  = "https://sso.redhat.com/auth/realms/redhat-external"
-	DefaultMasAuthURL  = "https://identity.api.openshift.com/auth/realms/rhoas-client-prod"
-	DefaultApiUrl   = "https://api.openshift.com"
-	DefaultClientID = "cloud-services"
+	DefaultAuthURL    = "https://sso.redhat.com/auth/realms/redhat-external"
+	DefaultMasAuthURL = "https://identity.api.openshift.com/auth/realms/rhoas-client-prod"
+	DefaultApiUrl     = "https://api.openshift.com"
+	DefaultClientID   = "cloud-services"
 )
 
 // Provider -
 func Provider() *schema.Provider {
 	return &schema.Provider{
 		Schema: map[string]*schema.Schema{
-			"offline_token": &schema.Schema{
+			"offline_token": {
 				Type:        schema.TypeString,
-				Optional:    true,
+				Optional:    false,
 				DefaultFunc: schema.EnvDefaultFunc("OFFLINE_TOKEN", nil),
 				Description: "The offline token is a refresh token with no expiry and can be used by non-interactive processes to provide an access token for Red Hat OpenShift Application Services. The offline token can be obtained from [https://cloud.redhat.com/openshift/token](https://cloud.redhat.com/openshift/token). As the offline token is a sensitive value that varies between environments it is best specified using the `OFFLINE_TOKEN` environment variable.",
 			},
-			"auth_url": &schema.Schema{
+			"auth_url": {
 				Type:        schema.TypeString,
 				Optional:    true,
 				DefaultFunc: schema.EnvDefaultFunc("AUTH_URL", DefaultAuthURL),
 				Description: fmt.Sprintf("The auth url is used to get an access token for the service by passing the offline token. By default %s is used.", DefaultAuthURL),
 			},
-			"client_id": &schema.Schema{
+			"client_id": {
 				Type:        schema.TypeString,
 				Optional:    true,
 				DefaultFunc: schema.EnvDefaultFunc("CLIENT_ID", DefaultClientID),
 				Description: fmt.Sprintf("The client id is used to when getting the access token using the offline token. By default %s is used.", DefaultClientID),
 			},
-			"api_url": &schema.Schema{
+			"api_url": {
 				Type:        schema.TypeString,
 				Optional:    true,
 				DefaultFunc: schema.EnvDefaultFunc("API_URL", DefaultApiUrl),
-				Description: fmt.Sprintf("The api url is used when managing resources. By default %s is used.", DefaultAuthURL),
+				Description: fmt.Sprintf("URL to the RHOAS services API. By default using production API:  %s.", DefaultApiUrl),
 			},
 		},
 		ResourcesMap: map[string]*schema.Resource{
@@ -82,14 +83,14 @@ func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}
 		Build()
 
 	if err != nil {
-		return nil,  diag.FromErr(err)
+		return nil, diag.FromErr(err)
 	}
 
 	err = c.RefreshTokens(ctx)
 
 	diags = append(diags, diag.Diagnostic{
-		Severity:      diag.Warning,
-		Summary:       "got refresh token",
+		Severity: diag.Warning,
+		Summary:  "got refresh token",
 	})
 
 	if err != nil {
