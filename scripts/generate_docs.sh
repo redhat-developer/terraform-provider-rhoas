247fe3fd68ed0f16d1d31140ea92e29a8a742bef
Jack Delahunt <jdealhunt@gmail.com>
2022-09-07 11:56:42 +0100

    refactor: docker container now does not run detached

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -3,4 +3,4 @@
 # run from the root directory of the project
 
 docker build --platform linux/amd64 -t rhoas_provider $(pwd)/scripts
-docker run --platform linux/amd64 --mount type=bind,source="$(pwd)",target=/terraform-provider-rhoas -d rhoas_provider
+docker run --platform linux/amd64 --mount type=bind,source="$(pwd)",target=/terraform-provider-rhoas rhoas_provider
