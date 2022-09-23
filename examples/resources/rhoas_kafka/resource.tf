819f1ac9b14b533f3501f059fee2ab49cbcf5c00
Juan Diaz Suarez <jdiazsua@redhat.com>
2022-09-23 12:37:02 +0200

    update rhoas_kafka example to match the current version of the provider

--- examples/resources/rhoas_kafka/resource.tf
+++ examples/resources/rhoas_kafka/resource.tf
@@ -1,19 +1,18 @@
 terraform {
   required_providers {
     rhoas = {
-      source  = "pmuir/rhoas"
+      source  = "registry.terraform.io/redhat-developer/rhoas"
+      version = "0.1"
     }
   }
 }
 
 provider "rhoas" {}
 
-/*resource "rhoas_kafka" "foo" {
-  kafka {
-    name = "foo"
-  }
+resource "rhoas_kafka" "foo" {
+  name = "foo"
 }
 
 output "bootstrap_server_foo" {
-  value = rhoas_kafka.foo.kafka[0].bootstrap_server_host
-}*/
+  value = rhoas_kafka.foo.bootstrap_server_host
+}
