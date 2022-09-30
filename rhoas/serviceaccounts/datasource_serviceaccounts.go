f67da0840f2a84bd398d1c671ec6903dd18e4959
Juan Diaz Suarez <jdiazsua@redhat.com>
2022-09-30 15:19:25 +0200

    fix the rhoas_service_accounts example

--- rhoas/serviceaccounts/datasource_serviceaccounts.go
+++ rhoas/serviceaccounts/datasource_serviceaccounts.go
@@ -11,7 +11,8 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"redhat.com/rhoas/rhoas-terraform-provider/m/rhoas/utils"
+
+	serviceaccountsclient "github.com/redhat-developer/app-services-sdk-go/serviceaccountmgmt/apiv1/client"
 )
 
 func DataSourceServiceAccounts() *schema.Resource {
@@ -29,10 +30,6 @@ func DataSourceServiceAccounts() *schema.Resource {
 							Computed:    true,
 							Description: "The client id associated with the service account",
 						},
-						"href": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
 						"description": {
 							Type:        schema.TypeString,
 							Computed:    true,
@@ -43,23 +40,18 @@ func DataSourceServiceAccounts() *schema.Resource {
 							Type:        schema.TypeString,
 							Computed:    true,
 						},
-						"kind": {
-							Type:        schema.TypeString,
-							Computed:    true,
-							Description: "The kind of resource in the Clients",
-						},
 						"name": {
 							Description: "The name of the service account",
 							Type:        schema.TypeString,
 							Computed:    true,
 						},
-						"owner": {
+						"created_by": {
 							Type:        schema.TypeString,
 							Computed:    true,
 							Description: "The username of the Red Hat account that owns the service account",
 						},
 						"created_at": {
-							Type:        schema.TypeString,
+							Type:        schema.TypeInt,
 							Computed:    true,
 							Description: "The RFC3339 date and time at which the service account was created",
 						},
@@ -88,21 +80,7 @@ func dataSourceKafkasRead(ctx context.Context, d *schema.ResourceData, m interfa
 		return diag.Errorf("%s%s", err.Error(), string(bodyBytes))
 	}
 
-	obj, err := utils.AsMap(data)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-
-	var raw []map[string]interface{}
-
-	// coerce the type
-	for _, item := range obj["items"].([]interface{}) {
-		raw = append(raw, item.(map[string]interface{}))
-	}
-
-	items := fixClientIDAndClientSecret(raw, nil)
-
-	if err := d.Set("service_accounts", items); err != nil {
+	if err := d.Set("service_accounts", flattenServiceAccountData(data)); err != nil {
 		return diag.FromErr(err)
 	}
 
@@ -110,4 +88,27 @@ func dataSourceKafkasRead(ctx context.Context, d *schema.ResourceData, m interfa
 	d.SetId(strconv.FormatInt(time.Now().Unix(), 10))
 
 	return diags
+}
+
+func flattenServiceAccountData(serviceAccounts []serviceaccountsclient.ServiceAccountData) []interface{} {
+	if serviceAccounts != nil {
+		sas := make([]interface{}, len(serviceAccounts), len(serviceAccounts))
+
+		for i := range serviceAccounts {
+			s := make(map[string]interface{})
+
+			s["client_id"] = serviceAccounts[i].GetClientId()
+			s["description"] = serviceAccounts[i].GetDescription()
+			s["id"] = serviceAccounts[i].GetId()
+			s["name"] = serviceAccounts[i].GetName()
+			s["created_by"] = serviceAccounts[i].GetCreatedBy()
+			s["created_at"] = serviceAccounts[i].GetCreatedAt()
+
+			sas[i] = s
+		}
+
+		return sas
+	}
+
+	return make([]interface{}, 0)
 }
